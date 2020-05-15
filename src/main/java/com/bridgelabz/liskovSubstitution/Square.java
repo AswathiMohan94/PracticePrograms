eb39018d2454fc4f174e90972496f18101034b38
AswathiMohan94 <aswathimohantly@gmail.com>
2020-05-15 20:46:30 +0530

    [Aswathi]ADDED : added example for liskovSubstitution principle

---
+++ src/main/java/com/bridgelabz/liskovSubstitution/Square.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.liskovSubstitution;
+
+public class Square extends Rectangle {
+
+    public void setBreadth(int breadth)
+    {
+        super.setBreadth(breadth);
+        super.setLength(breadth);
+    }
+
+    public void setLength(int length)
+    {
+        super.setLength(length);
+        super.setBreadth(length);
+    }
+}
